5be769957d5dbd378516151bbe12035c34a5fedd
Kim-02 <curry2002@koreatech.ac.kr>
2024-08-24 00:42:47 +0900

    commit
    과목을 엑셀에서 불러오는 UI까지 완성
    studentUI는 수정해야함.

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.my_view, name='my_view'),
-    path('test_view/', views.test_view, name='test_view'),
+    path('student_UI_view/', views.student_UI_view, name='student_UI_view'),
 ]
